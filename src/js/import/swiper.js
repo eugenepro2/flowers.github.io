10ff408686fb6cac81ed4670e01b1944e17c165e
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-08-27 20:50:59 +0300

    fix

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -20,25 +20,3 @@ var mySwiper = new Swiper('.swiper-intro', {
   },
   
 });
-
-// // mySwiper.slideNext();
-
-// //home-shares
-// var swiper2 = new Swiper('.swiper-intro-max', {
-//   slidesPerView: 1,
-//   spaceBetween: 0,
-//   observer: true,
-//   observeParents: true,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.swiper-intro-next',
-//     prevEl: '.swiper-intro-prev',
-//   },
-//   breakpoints: {
-//     767: {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//       loop: true
-//     }
-//   }
-// });
